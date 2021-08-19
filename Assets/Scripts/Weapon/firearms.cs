101edbd0ce634b7c6e12d2b29e057e4d23642238
Qian Zhangyu <xqqxzzxyy@gmail.com>
2021-08-19 23:47:23 +0900

    1

--- Assets/Scripts/Weapon/firearms.cs
+++ Assets/Scripts/Weapon/firearms.cs
@@ -9,14 +9,34 @@ namespace Scripts.Weapon
 
         public ParticleSystem MuzzleParticle;
         public ParticleSystem CasingParticle;
-
+        public float FireRate;
         public int AmmoInMag = 30;
         public int MaxAmmoCarried = 120;
 
         protected Animator GunAnimator;
+        private float lastFireTime;
+        protected int CurrentAmmo;
+        protected int CurrentMaxAmmo;
+
+
+        protected virtual void Start()
+        {
+            CurrentAmmo = AmmoInMag;
+            CurrentMaxAmmo = MaxAmmoCarried;       
+        }
         public void DoAttack()
         {
+            if (CurrentAmmo <= 0) return;
+            CurrentAmmo -= 1;
+            Shooting();
+            lastFireTime = Time.time;
+        }
+        protected abstract void Shooting();
+        protected abstract void Reload();
 
+        private bool isAllowShooting() 
+        {
+            return Time.time - lastFireTime > 1 / FireRate;
         }
     } 
 }
